f002009cd008bf06241cb1ac303552509c7b08f9
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2023-12-11 23:54:15 +0900

    feat: 開くメニューを作成

--- portfolio-frontend/src/app/_component/molecule/Header.tsx
+++ portfolio-frontend/src/app/_component/molecule/Header.tsx
@@ -15,7 +15,10 @@ export default function Header({}: Props) {
                 <Image className="bg-color4 text-color4" width={180} height={30} src="logo.svg" alt="asuka1975's logo" />
             </div>
             <div>
-                <MenuButton className="w-8 h-8" />
+                <MenuButton className="peer w-8 h-8" />
+                <div className="absolute bg-color5 bottom-0 left-0 w-screen h-0 peer-active:h-[87%] transition-all duration-500">
+
+                </div>
             </div>
         </header>
     )
