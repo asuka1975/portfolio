ba849249eb23a7f645d5855521e1954684f4c5ae
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2023-12-15 00:20:59 +0900

    feat: threeの各種補助ライブラリ導入

--- portfolio-frontend/src/app/_component/organism/BackgroundAnimation.tsx
+++ portfolio-frontend/src/app/_component/organism/BackgroundAnimation.tsx
@@ -2,41 +2,52 @@
 
 import { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Mesh } from 'three';
+import { DepthOfField, Bloom, Noise, Vignette, EffectComposer } from '@react-three/postprocessing';
+import { Mesh, Fog, Color } from 'three';
+import { MeshDistortMaterial } from '@react-three/drei';
 
-type BoxProps = {
+
+type SphereProps = {
     position: [x: number, y: number, z: number];
+    scale: number
 };
 
-const Box: React.FC<BoxProps> = (props) => {
+const Sphere: React.FC<SphereProps> = (props) => {
     const mesh = useRef<Mesh>(null!);
     const [hovered, setHover] = useState(false);
     const [active, setActive] = useState(false);
-    useFrame(() => (mesh.current.rotation.x += 0.01));
+    useFrame(() => (mesh.current.rotation.y -= 0.005));
 
     return (
         <mesh
             {...props}
             ref={mesh}
-            scale={active ? 1.5 : 1}
+            scale={props.scale}
             onClick={() => setActive(!active)}
             onPointerOver={() => setHover(true)}
             onPointerOut={() => setHover(false)}
         >
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+            <sphereGeometry args={[1, 12, 12]} />
+            <meshBasicMaterial wireframe={true} color={0xEEEEEE} />
         </mesh>
     );
 };
 
 export default function BackgroundAnimation() {
     return (
-        <div className="fix w-screen h-screen">
+        <div className="fixed w-screen h-screen z-0">
             <Canvas>
                 <ambientLight />
                 <pointLight position={[10, 10, 10]} />
-                <Box position={[-1.2, 0, 0]} />
-                <Box position={[1.2, 0, 0]} />
+                <color attach={"background"} args={["#F2F2F2"]} />
+                <Sphere position={[-2, 0, 0]} scale={4} />
+                <Sphere position={[3, 0, -1]} scale={4} />
+                <EffectComposer>
+                    <DepthOfField  />
+                    {/* <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
+                    <Noise opacity={0.02} />
+                    <Vignette eskil={false} offset={0.1} darkness={1.1} /> */}
+                </EffectComposer>
             </Canvas>
         </div>
     )
